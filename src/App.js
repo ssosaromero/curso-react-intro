52c8e0d996138fe72e6e390907deeb806382211e
Sergio Sosa Romero <smatias.sosa@gmail.com>
2023-05-29 10:41:28 +0200

    componentes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import logo from './platzi.webp';
 import './App.css';
 
+//esto no es HTML, es jsx
+//la "funcion" con mayusc es en realidad un componente
+//el componente tiene elementos adentro
+//el componente recibe propiedades
+//TodoItem es un componente adentro de otro componente(App)
+
 function App() {
   return (
     <div className="App">
+
+        <TodoItem/>
+        <TodoItem/>
+        <TodoItem/>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -15,11 +26,22 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Aprendamos React
         </a>
       </header>
     </div>
   );
 }
+
+  function TodoItem() {
+    return(
+      <li>
+        <span>V</span>
+        <p>Llorar con la Llorona</p>
+        <span>X</span>
+      </li>
+    );
+
+  }
 
 export default App;
