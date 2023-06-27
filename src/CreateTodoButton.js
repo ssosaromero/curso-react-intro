4b8194d0d3bd16e42299dae8f5689f63e2425c81
Sergio Sosa Romero <smatias.sosa@gmail.com>
2023-06-27 02:57:33 +0200

    se agregan eventos en search y create

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -1,8 +1,17 @@
 import './CreateTodoButton.css';
 
 function CreateTodoButton() {
-  return(
-    <button className= "CreateTodoButton">+</button>
+  return (
+    <button
+      className="CreateTodoButton"
+      onClick={
+        (event) => {
+          console.log('le diste click')
+          console.log(event)
+          console.log(event.target)
+        }
+      }
+    >+</button>
   );
 }
 
