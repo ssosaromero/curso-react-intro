66fe0098b9a62c7f058687f0cdc2ef18cb830356
Sergio Sosa Romero <smatias.sosa@gmail.com>
2023-06-29 03:04:07 +0200

    contando TODOs incompleto

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import './TodoSearch.css';
 
-function TodoSearch() {
-  const [searchValue, setSearchValue] = React.useState('');
+function TodoSearch({searchValue, setSearchValue})
 
-  console.log('los usuarios buscan todos de ' +
-  searchValue);
 
-  return (
+{return (
     <input
       placeholder="Cortar cebolla"
       className="TodoSearch"
